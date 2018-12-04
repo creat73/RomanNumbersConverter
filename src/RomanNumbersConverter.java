b54069a524062fa962d1dc3c8f6126d97a60006e
creat73 <nikill1125@gmail.com>
2018-12-04 19:21:29 +0100

    Add Convert method

--- src/RomanNumbersConverter.java
+++ src/RomanNumbersConverter.java
@@ -1,4 +1,10 @@
 
 public class RomanNumbersConverter {
     private final String[] ROMAN_VALUES = new String[] {"I", "V", "X", "L", "C", "D", "M"};
+
+    public String Convert(int number){
+        String romanNumber = "";
+        return romanNumber;
+    }
+
 }
