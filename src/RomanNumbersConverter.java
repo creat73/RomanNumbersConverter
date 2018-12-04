2e6d6e7839c1dcf4abc66f8bb5257ccbbace2e79
creat73 <nikill1125@gmail.com>
2018-12-04 19:28:03 +0100

    Add conversion limits

--- src/RomanNumbersConverter.java
+++ src/RomanNumbersConverter.java
@@ -1,6 +1,7 @@
 
 public class RomanNumbersConverter {
     private final String[] ROMAN_VALUES = new String[] {"I", "V", "X", "L", "C", "D", "M"};
+    // Conversion limitation due to lack of higher roman symbols
     private final int conversionUpperLimit = 4999;
     private final int conversionLowerLimit = 1;
 
